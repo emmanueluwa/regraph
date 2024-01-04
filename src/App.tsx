8b320a977980050a55beb114e0186aff1cf186c5
emmanueluwa <emmanuel@fulodev.com>
2024-01-04 14:30:09 +0000

    adding ability to create new customer using graphql usemutation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, useMutation, gql } from "@apollo/client";
 
 export type Customer = {
   id: number;
@@ -18,11 +18,38 @@ const GET_DATA = gql`
   }
 `;
 
+const CREATE_CUSTOMER = gql`
+  mutation CREATE_CUSTOMER($name: String!, $industry: String!) {
+    createCustomer(name: $name, industry: $industry) {
+      customer {
+        id
+        name
+      }
+    }
+  }
+`;
+
 function App() {
+  //form data
+  const [name, setName] = useState<string>("");
+  const [industry, setIndustry] = useState<string>("");
+
   const { loading, error, data } = useQuery(GET_DATA);
+
+  const [
+    createCustomer,
+    {
+      loading: createCustomerLoading,
+      error: createCustomerError,
+      data: createCustomerData,
+    },
+  ] = useMutation(CREATE_CUSTOMER, {
+    refetchQueries: [{ query: GET_DATA }],
+  });
 
   useEffect(() => {
     console.log(loading, error, data);
+    console.log(createCustomerLoading, createCustomerError, createCustomerData);
   });
   return (
     <div>
@@ -36,6 +63,45 @@ function App() {
             );
           })
         : null}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          createCustomer({ variables: { name: name, industry: industry } });
+          if (!error) {
+            setName("");
+            setIndustry("");
+          }
+        }}
+      >
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="industy">Industry</label>
+          <input
+            id="industry"
+            type="text"
+            value={industry}
+            onChange={(e) => {
+              setIndustry(e.target.value);
+            }}
+          />
+        </div>
+
+        <button disabled={createCustomerLoading ? true : false}>
+          Add Customer
+        </button>
+        {createCustomerError ? <p>Error creating customer</p> : null}
+      </form>
     </div>
   );
 }
